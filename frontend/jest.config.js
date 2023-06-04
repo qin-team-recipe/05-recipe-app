a45128411184f6e5285bc8f8527310cf7d2fa5d7
ゆうご <yuhgo090764@gmail.com>
2023-06-04 16:32:40 +0900

    chore: install jest

---
+++ frontend/jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: '.',
+})
+
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testEnvironment: 'jsdom',
+  testRegex: '(/__tests__/.*|(\\.|/)(test))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+}
+
+module.exports = createJestConfig(customJestConfig)
