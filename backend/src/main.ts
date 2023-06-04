703d50f9b7ec31299a0c1be48573ca3d0948e28f
Kazuki1131 <onetsubyo@gmail.com>
2023-06-04 18:51:01 +0900

    chore: add nest/config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from 'src/infrastructure/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const prismaService = app.get(PrismaService);
+  const prismaService = app.get(PrismaService, { strict: false });
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('APP_PORT') || 8080);
 }
 bootstrap();
