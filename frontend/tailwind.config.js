a7bce95acf6bf26834d3ad68f0572096bfd6aac3
ゆうご <yuhgo090764@gmail.com>
2023-06-07 23:41:24 +0900

    chore: box-shadowの設定を追加

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        base: "0px -2px 4px 0px rgba(0, 0, 0, 0.12)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
