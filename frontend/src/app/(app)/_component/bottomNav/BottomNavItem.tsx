8ffd879d91e21755a3ebb5a77dfeeb9ef243fbeb
Hetchma <hetchma.game@gmail.com>
2023-06-09 22:08:18 +0900

    feat:fix bottom-nav-menu & add bg color #8

--- frontend/src/app/(app)/_component/bottomNav/BottomNavItem.tsx
+++ frontend/src/app/(app)/_component/bottomNav/BottomNavItem.tsx
@@ -18,15 +18,14 @@ type BottomNavItemProps<T extends string> = {
 export const BottomNavItem: FC<BottomNavItemProps<string>> = (props) => {
   const { href, navLabel } = props
   const pathname = usePathname()
-  let textColor = ""
-  let imgColor = ""
-  if (pathname === href) {
-    textColor = "text-tomato-11"
-    imgColor = "stroke-tomato-11"
-  } else {
-    textColor = "text-gray-11 hover:text-tomato-11 stroke-gray-11 hover:stroke-tomato-11"
-    imgColor = "stroke-gray-11 group-hover:stroke-tomato-11"
-  }
+  const textColor =
+    pathname === href
+      ? "text-tomato-11 dark:text-tomatodark-11"
+      : "text-gray-11 hover:text-tomato-11 stroke-gray-11 hover:stroke-tomato-11 dark:text-graydark-11 dark:hover:text-tomatodark-11 dark:stroke-graydark-11 dark:hover:stroke-tomatodark-11"
+  const imgColor =
+    pathname === href
+      ? "stroke-tomato-11 dark:stroke-tomatodark-11"
+      : "stroke-gray-11 group-hover:stroke-tomato-11 dark:stroke-graydark-11 darkroup-hover:stroke-tomatodark-11"
 
   return (
     <Link href={href} className={`group flex flex-col items-center justify-center gap-1 ${textColor}`}>
