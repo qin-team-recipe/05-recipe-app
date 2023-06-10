c744cf43933fe5defae8ff92b5a52f5bf84309ed
ゆうご <yuhgo090764@gmail.com>
2023-06-10 14:19:53 +0900

    feat: 注目のレシピセクションを作成

--- frontend/src/app/(app)/search/page.tsx
+++ frontend/src/app/(app)/search/page.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { HotChef } from "@/app/(app)/search/_component"
+
 export const metadata = {
   title: "シェフやレシピを検索",
 }
@@ -12,7 +14,7 @@ export default function SearchPage() {
   return (
     <div className="space-y-6">
       <section>
-        <h3>注目のシェフ</h3>
+        <HotChef />
       </section>
 
       <section>
