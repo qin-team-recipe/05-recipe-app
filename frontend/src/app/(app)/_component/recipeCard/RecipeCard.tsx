e827b93e0923927948559447db96fe97bcb6bb47
Hetchma <hetchma.game@gmail.com>
2023-06-10 23:37:18 +0900

    feat:create recipe-fav-button & add svgicon props

--- frontend/src/app/(app)/_component/recipeCard/RecipeCard.tsx
+++ frontend/src/app/(app)/_component/recipeCard/RecipeCard.tsx
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { FC } from "react"
 
-export const RecipeCard = () => {
+import { RecipeFavButton } from "@/app/(app)/_component/recipeCard/RecipeFavButton"
+
+export const RecipeCard: FC = () => {
   return (
     <div className="text-mauve-normal w-36">
-      <div className="bg-tomato-ui aspect-square w-full rounded-xl"></div>
+      <div className="bg-tomato-ui relative aspect-square w-full rounded-xl bg-[url('/pizza.jpg')] bg-cover bg-center bg-no-repeat">
+        <RecipeFavButton />
+      </div>
       <div className="p-1">
         <div className="text-sm font-bold line-clamp-2">
           メイン文章メイン文章メイン文章メイン文章メイン文章メイン文章メイン文章
