6afbc0adad5399146e608338bd3e4e5488d96f0a
ゆうご <yuhgo090764@gmail.com>
2023-06-11 15:53:32 +0900

    refactor: コンテナの修正に伴い、それぞれの要素をコンテナでwrap

--- frontend/src/app/(app)/search/_component/HotChef.tsx
+++ frontend/src/app/(app)/search/_component/HotChef.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 
 import { ScrollAreaWrapper } from "@/app/_component"
+import { ContentContainer } from "@/app/(app)/_component/container"
 import { SectionHeader } from "@/app/(app)/_component/header"
 
 /** @package */
 export const HotChef = () => {
-  const chefCards = Array.from({ length: 50 }).map((_, i) => {
+  const chefCards = Array.from({ length: 10 }).map((_, i) => {
     return (
       <div key={i} className="min-w-32 h-56 w-32 bg-tomato-5">
         山田シェフ
@@ -15,10 +16,19 @@ export const HotChef = () => {
 
   return (
     <div className="space-y-4">
-      <SectionHeader title="注目のシェフ" href="/favorite" isMore />
-      <ScrollAreaWrapper>
-        <div className="flex space-x-4">{chefCards}</div>
-      </ScrollAreaWrapper>
+      <ContentContainer>
+        <SectionHeader title="注目のシェフ" href="/favorite" />
+      </ContentContainer>
+
+      {/* 横スクロールエリアには右paddingをつけない */}
+      <ContentContainer isPaddingRight={false}>
+        <ScrollAreaWrapper>
+          {/* カードリストの右にだけpaddingがつくように */}
+          <ContentContainer isPaddingLeft={false}>
+            <div className="flex space-x-4">{chefCards}</div>
+          </ContentContainer>
+        </ScrollAreaWrapper>
+      </ContentContainer>
     </div>
   )
 }
