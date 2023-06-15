4f5e6ffe2793a026354f09fbfdf6d3610db0b24f
Hetchma <hetchma.game@gmail.com>
2023-06-15 23:53:55 +0900

    feat:add back-button to chef-hero

--- frontend/src/app/(app)/_component/header/PageDetailHeader.tsx
+++ frontend/src/app/(app)/_component/header/PageDetailHeader.tsx
@@ -2,34 +2,33 @@ import React, { FC } from "react"
 import Image from "next/image"
 
 import { ContentContainer } from "@/app/(app)/_component/container"
-import { ChefFavButton } from "@/app/(app)/chef/[chefId]/_component"
+import { BackButton, ChefFavButton } from "@/app/(app)/chef/[chefId]/_component"
 
 /** @package */
 export const PageDetailHeader: FC = () => {
   return (
-    <>
-      <div>
-        <div className="relative h-96 max-h-screen w-full">
-          <Image src="/chef.jpg" alt="chef" fill className="h-3/4 object-cover" />
-        </div>
-        <ContentContainer>
-          <div className="text-mauve-normal">
-            <h1 className="pt-4 text-2xl font-bold">山田シェフ</h1>
-            <div className="py-4">
-              初の絵本出版！『まねっこシェフ』・ふわふわ！スクランブルエッグ・にぎにぎ！おにぎり主婦の友社より３月３日、２冊同時発売！絶賛発売中！
+    <div>
+      <div className="relative h-96 max-h-screen w-full">
+        <Image src="/chef.jpg" alt="chef" fill className="h-3/4 object-cover" />
+        <BackButton />
+      </div>
+      <ContentContainer>
+        <div className="text-mauve-normal">
+          <h1 className="pt-4 text-2xl font-bold">山田シェフ</h1>
+          <div className="py-4">
+            初の絵本出版！『まねっこシェフ』・ふわふわ！スクランブルエッグ・にぎにぎ！おにぎり主婦の友社より３月３日、２冊同時発売！絶賛発売中！
+          </div>
+          <div className="align-center text-mauve-dim flex gap-2 text-sm">
+            <div>
+              <span className="text-mauve-normal pr-1 font-bold">2,345</span>レシピ
             </div>
-            <div className="align-center text-mauve-dim flex gap-2 text-sm">
-              <div>
-                <span className="text-mauve-normal pr-1 font-bold">2,345</span>レシピ
-              </div>
-              <div>
-                <span className="text-mauve-normal pr-1 font-bold">1,234</span>フォロワー
-              </div>
+            <div>
+              <span className="text-mauve-normal pr-1 font-bold">1,234</span>フォロワー
             </div>
           </div>
-          <ChefFavButton />
-        </ContentContainer>
-      </div>
-    </>
+        </div>
+        <ChefFavButton />
+      </ContentContainer>
+    </div>
   )
 }
