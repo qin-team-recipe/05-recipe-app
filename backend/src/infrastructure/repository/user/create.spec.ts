b82e1acec47605b94954a3bc5ad5cc2def4ed97a
Kazuki1131 <onetsubyo@gmail.com>
2023-06-17 05:12:44 +0900

    test: update for user model change

--- backend/src/infrastructure/repository/user/create.spec.ts
+++ backend/src/infrastructure/repository/user/create.spec.ts
@@ -38,12 +38,6 @@ describe('UserRepository.create()', () => {
     // Exercise: call the function
     const userProps: UserCreateInput = {
       email: 'test@test.com',
-      nickname: 'John Doe',
-      imgPath: 'test/path',
-      introduction: 'test introduction',
-      twitterId: 'test_twitter_id',
-      instagramId: 'test_instagram_id',
-      siteUrl: 'https://test_site_url.com',
     };
     const user = await repository.create(userProps);
 
@@ -54,20 +48,12 @@ describe('UserRepository.create()', () => {
 
     // Verify: ensure the function returns the data we specified
     expect(user.email).toStrictEqual(userProps.email);
-    expect(user.nickname).toStrictEqual(userProps.nickname);
-    expect(user.imgPath).toStrictEqual(userProps.imgPath);
-    expect(user.introduction).toStrictEqual(userProps.introduction);
-    expect(user.twitterId).toStrictEqual(userProps.twitterId);
-    expect(user.instagramId).toStrictEqual(userProps.instagramId);
-    expect(user.siteUrl).toStrictEqual(userProps.siteUrl);
   });
 
   test('Create user without optional values', async () => {
     // Exercise: call the function
     const userPropsWithoutOptional: UserCreateInput = {
-      nickname: 'John Doe',
       email: 'test@test.com',
-      introduction: 'test introduction',
     };
     const user = await repository.create(userPropsWithoutOptional);
 
@@ -77,10 +63,6 @@ describe('UserRepository.create()', () => {
     });
 
     // Verify: ensure the function returns the data we specified
-    expect(user.nickname).toStrictEqual(userPropsWithoutOptional.nickname);
     expect(user.email).toStrictEqual(userPropsWithoutOptional.email);
-    expect(user.introduction).toStrictEqual(
-      userPropsWithoutOptional.introduction,
-    );
   });
 });
--- backend/src/infrastructure/repository/user/update.spec.ts
+++ backend/src/infrastructure/repository/user/update.spec.ts
@@ -1,20 +1,24 @@
 import { Test } from '@nestjs/testing';
 import updatePrismaMock from 'prisma-mock';
 import { CustomLoggerService } from 'src/common/logger/custom-logger.service';
-import { UserResponse, UserUpdateInput } from 'src/entity/user.entity';
+import { UserUpdateInput } from 'src/entity/user.entity';
 import { OrmClient } from 'src/infrastructure/orm/orm.client';
 import { UserRepository } from './repository';
 
 let repository: UserRepository;
 let ormMock: OrmClient;
-let loggerMock: Partial<CustomLoggerService>;
-let createdResult: UserResponse;
-let createdUser: UserResponse;
+
+const updateResult = {
+  id: 'cuid',
+  email: 'initial@test.com',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
 
 beforeAll(async () => {
   ormMock = updatePrismaMock();
 
-  loggerMock = {
+  const loggerMock = {
     error: jest.fn(),
   };
 
@@ -34,33 +38,12 @@ beforeAll(async () => {
 
   repository = moduleRef.get<UserRepository>(UserRepository);
 
-  // Create a test user and mock the user.create method
-  const testUserProps = {
-    email: 'initial@test.com',
-    nickname: 'Initial User',
-    imgPath: 'initial/path',
-    introduction: 'initial introduction',
-    twitterId: 'initial_twitter_id',
-    instagramId: 'initial_instagram_id',
-    siteUrl: 'https://initial_site_url.com',
-  };
-
-  createdResult = {
-    id: 'cuid',
-    ...testUserProps,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  };
-
-  ormMock.user.create = jest.fn().mockResolvedValue(createdResult);
-  createdUser = await repository.create(testUserProps);
-
   // Mock user.update method
   ormMock.user.update = jest.fn().mockImplementation((updateProps) => {
     // If the user with the given ID exists, return the updated user
-    if (updateProps.where.id === createdResult.id) {
+    if (updateProps.where.id === updateResult.id) {
       return Promise.resolve({
-        ...createdResult,
+        ...updateResult,
         ...updateProps.data,
       });
     }
@@ -75,9 +58,7 @@ describe('UserRepository.update()', () => {
     // Exercise: call the function
     const userProps: UserUpdateInput = {
       id: undefined,
-      nickname: 'John Doe',
       email: 'test@test.com',
-      introduction: 'test introduction',
     };
     const user = repository.update(userProps);
     await expect(user).rejects.toThrowError(Error('id must be a string'));
@@ -87,9 +68,7 @@ describe('UserRepository.update()', () => {
     // Exercise: call the function
     const userProps: UserUpdateInput = {
       id: 'non-existent-id',
-      nickname: 'John Doe',
       email: 'test@test.com',
-      introduction: 'test introduction',
     };
     const user = await repository.update(userProps);
 
@@ -97,17 +76,11 @@ describe('UserRepository.update()', () => {
     expect(user).toBeNull();
   });
 
-  test('Update user with all values', async () => {
+  test('Update user', async () => {
     // Exercise: call the function
     const userProps: UserUpdateInput = {
-      id: createdUser.id,
+      id: updateResult.id,
       email: 'test@test.com',
-      nickname: 'John Doe',
-      imgPath: 'test/path',
-      introduction: 'test introduction',
-      twitterId: 'test_twitter_id',
-      instagramId: 'test_instagram_id',
-      siteUrl: 'https://test_site_url.com',
     };
     const user = await repository.update(userProps);
 
@@ -120,40 +93,5 @@ describe('UserRepository.update()', () => {
     // Verify: ensure the function returns the data we specified
     expect(user?.id).toStrictEqual(userProps.id);
     expect(user?.email).toStrictEqual(userProps.email);
-    expect(user?.nickname).toStrictEqual(userProps.nickname);
-    expect(user?.imgPath).toStrictEqual(userProps.imgPath);
-    expect(user?.introduction).toStrictEqual(userProps.introduction);
-    expect(user?.twitterId).toStrictEqual(userProps.twitterId);
-    expect(user?.instagramId).toStrictEqual(userProps.instagramId);
-    expect(user?.siteUrl).toStrictEqual(userProps.siteUrl);
-  });
-
-  test('Update user without optional values', async () => {
-    // Exercise: call the function
-    const userPropsWithoutOptional: UserUpdateInput = {
-      id: createdUser.id,
-      nickname: 'John Doe',
-      email: 'test@test.com',
-      introduction: 'test introduction',
-    };
-    const user = await repository.update(userPropsWithoutOptional);
-
-    // Verify: ensure user.update was called with correct arguments
-    expect(ormMock.user.update).toHaveBeenCalledWith({
-      where: { id: userPropsWithoutOptional.id },
-      data: userPropsWithoutOptional,
-    });
-
-    // Verify: ensure the function returns the data we specified
-    expect(user?.id).toStrictEqual(userPropsWithoutOptional.id);
-    expect(user?.email).toStrictEqual(userPropsWithoutOptional.email);
-    expect(user?.nickname).toStrictEqual(userPropsWithoutOptional.nickname);
-    expect(user?.introduction).toStrictEqual(
-      userPropsWithoutOptional.introduction,
-    );
-    expect(user?.imgPath).toStrictEqual(createdUser.imgPath);
-    expect(user?.twitterId).toStrictEqual(createdUser.twitterId);
-    expect(user?.instagramId).toStrictEqual(createdUser.instagramId);
-    expect(user?.siteUrl).toStrictEqual(createdUser.siteUrl);
   });
 });
