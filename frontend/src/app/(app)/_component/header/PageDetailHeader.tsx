4a5c93c5ebf1bfe959aedfc801ba17a48ed4c806
ゆうご <yuhgo090764@gmail.com>
2023-06-16 18:36:26 +0900

    Merge branch 'main' of https://github.com/qin-team-recipe/05-recipe-app into feat/two-tab

--- frontend/src/app/(app)/_component/header/PageDetailHeader.tsx
+++ frontend/src/app/(app)/_component/header/PageDetailHeader.tsx
@@ -21,8 +21,6 @@ export const PageDetailHeader: FC = () => {
           <div className="align-center text-mauve-dim flex gap-2 text-sm">
             <div>
               <span className="text-mauve-normal pr-1 font-bold">2,345</span>レシピ
-            </div>
-            <div>
               <span className="text-mauve-normal pr-1 font-bold">1,234</span>フォロワー
             </div>
           </div>
