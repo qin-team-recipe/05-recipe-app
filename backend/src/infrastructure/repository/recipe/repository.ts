920b0bbcc14de4000ae608d378fe4eea3ebd5615
kkkkoki <koki_inoue45@icloud.com>
2023-06-18 19:05:16 +0900

    feat: add recipe update repository&test

--- backend/src/infrastructure/repository/recipe/repository.ts
+++ backend/src/infrastructure/repository/recipe/repository.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { RecipeCreateInput, RecipeResponse } from 'src/entity/recipe.entity';
+import {
+  RecipeCreateInput,
+  RecipeResponse,
+  RecipeUpdateInput,
+} from 'src/entity/recipe.entity';
 import { OrmClient } from 'src/infrastructure/orm/orm.client';
 import { prismaErrorHandling } from 'src/infrastructure/repository/prisma-error-handling';
+import { InvalidArgsError } from 'src/utils/exception/invalid-args.error';
 import { CustomLoggerService } from 'src/utils/logger/custom-logger.service';
 
 @Injectable()
@@ -15,6 +20,27 @@ export class RecipeRepository {
   async create(recipeProps: RecipeCreateInput): Promise<RecipeResponse> {
     try {
       return await this.orm.recipe.create({ data: recipeProps });
+    } catch (error) {
+      this.logger.error(error);
+      prismaErrorHandling(error);
+      throw error;
+    }
+  }
+
+  // レシピを更新する
+  async update(recipeProps: RecipeUpdateInput): Promise<RecipeResponse | null> {
+    if (typeof recipeProps.id !== 'string') {
+      const error = new InvalidArgsError('id must be a string');
+      this.logger.error(error);
+      prismaErrorHandling(error);
+      throw error;
+    }
+
+    try {
+      return await this.orm.recipe.update({
+        where: { id: recipeProps.id },
+        data: recipeProps,
+      });
     } catch (error) {
       this.logger.error(error);
       prismaErrorHandling(error);
