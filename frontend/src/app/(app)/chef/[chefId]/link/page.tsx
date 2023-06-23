6382edc41e1929fa598a97fdf67452fc3c85272e
bmstyojaop <bmbdyojaop@gmail.com>
2023-06-23 21:00:07 +0900

    ダミー用のLinkコンポーネントを追加

--- frontend/src/app/(app)/chef/[chefId]/link/page.tsx
+++ frontend/src/app/(app)/chef/[chefId]/link/page.tsx
@@ -5,6 +5,8 @@ import { PageDetailHeader } from "@/app/(app)/_component/header"
 import { TwoTab } from "@/app/(app)/_component/twoTab"
 import { twoTabLinkList } from "@/app/(app)/chef/[chefId]/_lib"
 
+import { LinkCard } from "../_component"
+
 export const metadata = {
   title: "シェフ詳細 | リンク",
 }
@@ -19,10 +21,45 @@ const LinkTabPage: FC<LinkTabPageProps> = (props) => {
   const { params } = props
 
   const chefCards = Array.from({ length: 4 }).map((_, i) => {
+    type DummydataType = {
+      accountId?: string
+      linkType: "twitter" | "instagram" | "others"
+      siteName?: string
+      snsFollower?: number
+      url: string
+    }
+
+    const website: DummydataType = {
+      accountId: undefined,
+      linkType: "others",
+      siteName: "",
+      snsFollower: undefined,
+      url: "",
+    }
+    if (i === 0) {
+      website.url = "https://about.twitter.com/ja"
+      website.linkType = "twitter"
+      website.accountId = "@hogehogeman"
+      website.snsFollower = 123
+    } else if (i === 1) {
+      website.url = "https://www.instagram.com/"
+      website.linkType = "instagram"
+      website.accountId = "@fugramaer"
+      website.snsFollower = 300
+    } else {
+      website.siteName = "ほげふーふーがどっとこむ"
+      website.url = "#"
+      website.linkType = "others"
+    }
     return (
-      <div key={i} className="h-20 w-full bg-tomato-5">
-        Linkカード
-      </div>
+      <LinkCard
+        key={i}
+        siteName={website.siteName}
+        url={website.url}
+        linkType={website.linkType}
+        snsFollower={website.snsFollower}
+        accountId={website.accountId}
+      />
     )
   })
 
@@ -31,7 +68,6 @@ const LinkTabPage: FC<LinkTabPageProps> = (props) => {
   return (
     <div>
       <PageDetailHeader />
-
       <div className="py-7">
         <TwoTab linkList={linkList}>
           <ContentContainer>
