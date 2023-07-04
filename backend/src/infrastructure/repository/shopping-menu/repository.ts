6b3b26ffda575488e29235edf7ba3e9db2f1ab19
saenagiumi <takeru.konno.cw@gmail.com>
2023-07-04 20:24:15 +0900

    feat(repository): add shopping-menu update

--- backend/src/infrastructure/repository/shopping-menu/repository.ts
+++ backend/src/infrastructure/repository/shopping-menu/repository.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@nestjs/common';
 import {
   ShoppingMenuCreateInput,
   ShoppingMenuResponse,
+  ShoppingMenuUpdateInput,
 } from 'src/entity/shopping-menu.entity';
 import { OrmClient } from 'src/infrastructure/orm/orm.client';
 import { prismaErrorHandling } from 'src/infrastructure/repository/prisma-error-handling';
+import { InvalidArgsError } from 'src/utils/exception/invalid-args.error';
 import { CustomLoggerService } from 'src/utils/logger/custom-logger.service';
 
 @Injectable()
@@ -20,6 +22,29 @@ export class ShoppingMenuRepository {
   ): Promise<ShoppingMenuResponse> {
     try {
       return await this.orm.shoppingMenu.create({ data: shoppingMenuProps });
+    } catch (error) {
+      this.logger.error(error);
+      prismaErrorHandling(error);
+      throw error;
+    }
+  }
+
+  // 買い物メニューを更新する
+  async update(
+    shoppingMenuProps: ShoppingMenuUpdateInput,
+  ): Promise<ShoppingMenuResponse | null> {
+    if (typeof shoppingMenuProps.id !== 'string') {
+      const error = new InvalidArgsError('id must be a string');
+      this.logger.error(error);
+      prismaErrorHandling(error);
+      throw error;
+    }
+
+    try {
+      return await this.orm.shoppingMenu.update({
+        where: { id: shoppingMenuProps.id },
+        data: shoppingMenuProps,
+      });
     } catch (error) {
       this.logger.error(error);
       prismaErrorHandling(error);
