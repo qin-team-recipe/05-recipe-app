66d80ce816785fba2fe3cd9a58a5adc639f9cf0f
ゆうご <yuhgo090764@gmail.com>
2023-07-11 22:15:50 +0900

    feat: 戻るボタンのスタイルのパターンを追加

--- frontend/src/app/(app)/_component/backButton/BackButton.tsx
+++ frontend/src/app/(app)/_component/backButton/BackButton.tsx
@@ -1,22 +1,56 @@
 "use client"
 
-import React from "react"
+import React, { FC } from "react"
+import { Route } from "next"
 import { useRouter } from "next/navigation"
 
 import { IconArrowLeft } from "@tabler/icons-react"
+import { tv } from "tailwind-variants"
+
+type BackButtonProps = {
+  href?: Route<string>
+  isFloating?: boolean
+}
+
+const backButton = tv({
+  slots: {
+    button: "static text-mauve-12",
+    icon: "h-8 w-8",
+  },
+  variants: {
+    isFloating: {
+      false: {
+        icon: "h-6 w-6",
+      },
+      true: {
+        button:
+          "absolute left-4 top-4 rounded-full bg-mauve-12 text-mauve-1 bg-opacity-20 h-8 w-8",
+      },
+    },
+  },
+})
 
 /** @package */
-export const BackButton = () => {
+export const BackButton: FC<BackButtonProps> = (props) => {
+  const { href, isFloating = false } = props
+
   const router = useRouter()
+
+  const { button, icon } = backButton({
+    isFloating,
+  })
 
   return (
     <button
-      className="absolute left-4 top-4"
+      className={button()}
       onClick={() => {
+        if (href) {
+          return router.push(href)
+        }
         return router.back()
       }}
     >
-      <IconArrowLeft size={36} color="white" />
+      <IconArrowLeft className={icon()} />
     </button>
   )
 }
