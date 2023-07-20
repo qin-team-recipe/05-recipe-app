39978cba9f822a1369a0ae4ee4f94e116a589d26
ゆうご <yuhgo090764@gmail.com>
2023-07-20 11:52:13 +0900

    feat: シェフに紐づくリンクアイコンを表示

--- frontend/src/app/(app)/_component/header/index.ts
+++ frontend/src/app/(app)/_component/header/index.ts
@@ -1,4 +1,4 @@
 export { SectionHeader } from "./SectionHeader"
 export { type SubButtonLink } from "./SectionHeader.types"
 export { PageHeader } from "./PageHeader"
-export type { Common, Chef, Recipe, MyPage } from "./PageDetail.types"
+export type { Common, Chef, Recipe, MyPage, Link } from "./PageDetail.types"
