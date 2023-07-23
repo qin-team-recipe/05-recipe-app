aee4c19693687aca8d9bd05b3d5292108d442575
Hetchma <hetchma.game@gmail.com>
2023-07-23 21:48:25 +0900

    fix:myPageDDMenu #110

--- frontend/src/app/(app)/myPage/_component/MyPageDDMenu.tsx
+++ frontend/src/app/(app)/myPage/_component/MyPageDDMenu.tsx
@@ -12,40 +12,33 @@ type MyPageDDMenuProps = {
   children: ReactNode
 }
 
-const ddMenu = tv({
+const baseDDMenu = tv({
   slots: {
     content:
       "absolute right-[-10px] top-0 min-w-[220px] rounded-md bg-mauve-1 p-1 shadow-lg",
     item: "cursor-pointer flex items-start gap-3 select-none rounded px-1 pb-1 pl-2 text-sm  text-mauve-11 outline-none hover:bg-mauve-3 hover:text-mauve-12",
     mainText: "h-5 pb-1 leading-5",
+    separator: "m-1 h-[1px] bg-mauve-5",
+    subText: "h-5 pb-1 text-xs leading-5",
   },
-  variants: {
-    hasSeparator: {
-      separator: "m-1 h-[1px] bg-mauve-5",
-    },
-    hasSubText: {
-      subText: "h-5 pb-1 text-xs leading-5",
-    },
-  },
+})
+
+const myPageDDMenu = tv({
+  extend: baseDDMenu,
 })
 
 export const MyPageDDMenu: FC<MyPageDDMenuProps> = (props) => {
   const { children } = props
 
-  const { content, item, mainText } = ddMenu({
-    hasSeparator: true,
-    hasSubText: true,
-  })
+  const { content, item, mainText } = myPageDDMenu()
 
   const menuContent = () => {
     return (
       <DropdownMenu.Content className={content()}>
-        {/* <DropdownMenu.Separator className={separator()} /> */}
         <DropdownMenu.Item className={item()}>
           <IconPencil width={20} height={20} />
           <div>
             <div className={mainText()}>プロフィールを編集する</div>
-            {/* <div className={subText()}>サブテキスト</div> */}
           </div>
         </DropdownMenu.Item>
         <DropdownMenu.Item className={item()}>
