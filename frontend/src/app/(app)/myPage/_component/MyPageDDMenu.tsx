bfdcb61dfd48d8e556b810fd3b62cef929368e0b
Hetchma <hetchma.game@gmail.com>
2023-07-30 17:05:35 +0900

    fix:メニュー間隔調整 #116

--- frontend/src/app/(app)/myPage/_component/MyPageDDMenu.tsx
+++ frontend/src/app/(app)/myPage/_component/MyPageDDMenu.tsx
@@ -15,7 +15,7 @@ type MyPageDDMenuProps = {
 const baseDDMenu = tv({
   slots: {
     content:
-      "absolute right-[-10px] top-0 min-w-[220px] rounded-md bg-mauve-1 p-1 shadow-lg",
+      "absolute right-[-10px] top-0 min-w-[220px] rounded-md bg-mauve-1 p-1 shadow-lg flex flex-col gap-1",
     item: "cursor-pointer flex items-start gap-3 select-none rounded px-1 pb-1 pl-2 text-sm  text-mauve-11 outline-none hover:bg-mauve-3 hover:text-mauve-12",
     mainText: "h-5 pb-1 leading-5",
     separator: "m-1 h-[1px] bg-mauve-5",
