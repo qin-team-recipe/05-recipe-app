badee23a2a963e55dbd32f6b576b2dfb5fda3333
Kazuki1131 <onetsubyo@gmail.com>
2023-08-05 17:57:34 +0900

    feat: add register auth provider function to user use case

---
+++ backend/src/use-case/user/use-case.ts
@@ -0,0 +1,47 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  UserCreateInput,
+  UserResponse,
+  UserUpdateInput,
+  UserWithAuthProvidersResponse,
+} from 'src/entity/user.entity';
+import { UserAuthProviderRepository } from 'src/infrastructure/repository/user-auth-provider/repository';
+import { UserRepository } from 'src/infrastructure/repository/user/repository';
+
+@Injectable()
+export class UserUseCase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly authRepository: UserAuthProviderRepository,
+  ) {}
+
+  // 認証プロバイダーの登録
+  async registerAuthProvider(
+    userProps: UserCreateInput,
+  ): Promise<UserWithAuthProvidersResponse> {
+    const user = await this.userRepository.findByEmail(userProps.email);
+
+    if (!user) {
+      return await this.userRepository.createWithAuthProvider(userProps);
+    }
+
+    const hasRequestAuthProvider = user.userAuthProviders.some(
+      (provider) => provider.provider === userProps.provider,
+    );
+
+    if (!hasRequestAuthProvider) {
+      const userAuth = await this.authRepository.create({
+        userId: user.id,
+        provider: userProps.provider,
+        providerId: userProps.providerId,
+      });
+
+      return {
+        ...user,
+        userAuthProviders: [...user.userAuthProviders, userAuth],
+      };
+    }
+
+    return user;
+  }
+}
