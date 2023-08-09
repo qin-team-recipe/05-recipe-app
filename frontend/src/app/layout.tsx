89474f4dc09470439624266389ad76a11f38a234
ゆうご <yuhgo090764@gmail.com>
2023-08-09 18:59:11 +0900

    fix: mswをアンインストール

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,18 +2,12 @@ import "./globals.css"
 
 import React, { ReactNode } from "react"
 
-import { server } from "@/mock"
-
 export const metadata = {
   title: "一流レシピ",
 }
 
 type RootLayoutProps = {
   children: ReactNode
-}
-
-if (process.env.NODE_ENV === "development") {
-  server.listen()
 }
 
 export default function RootLayout(props: RootLayoutProps) {
