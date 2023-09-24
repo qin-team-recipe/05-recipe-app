9917234a4089d8bf39206afafcb72a7f56f263b5
kkkkoki <kokiinoue45@gmail.com>
2023-09-24 17:48:33 +0900

    refactor: getRandomInt関数をrootのlibに移動

--- frontend/src/app/(app)/recipe/[recipeId]/api/route.ts
+++ frontend/src/app/(app)/recipe/[recipeId]/api/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server"
 
+import { getRandomInt } from "@/app/_lib"
 import { type Recipe } from "@/app/(app)/_component/header"
 
 export const GET = async (
@@ -7,12 +8,6 @@ export const GET = async (
   { params }: { params: { recipeId: string } },
 ) => {
   const recipeId = params.recipeId
-
-  // 「1」〜「渡した引数の数字」までのランダムな「number」を生成してreturnする関数（少しリッチな見た目にするため）
-  const getRandomInt = (max: number) => {
-    return Math.floor(Math.random() * max) + 1
-  }
-
   const data: Recipe = {
     favoriteCount: getRandomInt(1000),
     img: `/recipe-images/recipe-${getRandomInt(5)}.jpg`,
