402a826f5a2b344b60696ec59687476c044ffadf
ゆうご <yuhgo090764@gmail.com>
2023-09-24 18:46:53 +0900

    feat: chefDataをlayout.tsxで取得するようにしたため、ChefPageDetailをを削除

--- frontend/src/app/(app)/chef/[chefId]/page.tsx
+++ frontend/src/app/(app)/chef/[chefId]/page.tsx
@@ -1,16 +1,9 @@
 import React, { FC } from "react"
 
-import { getChef } from "@/mock/api"
-
 import { ContentContainer } from "@/app/(app)/_component/container"
 import { RecipeCard } from "@/app/(app)/_component/recipeCard"
 import { Tab } from "@/app/(app)/_component/tab"
-import { ChefPageDetail } from "@/app/(app)/chef/[chefId]/_component"
 import { tabLinkList } from "@/app/(app)/chef/[chefId]/_lib"
-
-export const metadata = {
-  title: "シェフ詳細 | シェフ一覧",
-}
 
 type ChefPageProps = {
   params: {
@@ -32,19 +25,14 @@ const ChefPage: FC<ChefPageProps> = async (props) => {
   })
 
   const linkList = tabLinkList(params.chefId)
-  const chefData = getChef(params.chefId)
 
   return (
-    <div>
-      <ChefPageDetail data={chefData} />
-
-      <div className="py-7">
-        <Tab linkList={linkList}>
-          <ContentContainer>
-            <div className="grid grid-cols-2 gap-2 pt-5">{recipeCards}</div>
-          </ContentContainer>
-        </Tab>
-      </div>
+    <div className="py-7">
+      <Tab linkList={linkList}>
+        <ContentContainer>
+          <div className="grid grid-cols-2 gap-2 pt-5">{recipeCards}</div>
+        </ContentContainer>
+      </Tab>
     </div>
   )
 }
